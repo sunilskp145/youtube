3dba1b0c772553d1754cdd4a477eeb9821a0f857
Sunil Kumar <Sunil.Kumar@target.com>
2023-03-21 15:51:01 +0530

    Values form properties file

--- src/main/java/com/youtube/video/service/VideoService.java
+++ src/main/java/com/youtube/video/service/VideoService.java
@@ -1,6 +1,7 @@
 package com.youtube.video.service;
 
 import com.youtube.video.response.VideoResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,15 +10,20 @@ import java.util.Collections;
 
 @Service
 public class VideoService {
+    @Value("${Key}")
+    private String key;
+
+    @Value("${Url}")
+    private String youtube_com;
     public VideoResponse getResponse(String query){
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-RapidAPI-Key", "6c7c740e26mshbb60c3723603750p16928djsn32d8cf836ce2");
+        headers.set("X-RapidAPI-Key", key);
         headers.set("X-RapidAPI-Host","yt-api.p.rapidapi.com");
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<VideoResponse> TaskResponse = new RestTemplate().exchange("https://yt-api.p.rapidapi.com/search?query=" +query, HttpMethod.GET, entity, VideoResponse.class);
+        ResponseEntity<VideoResponse> TaskResponse = new RestTemplate().exchange(youtube_com +query, HttpMethod.GET, entity, VideoResponse.class);
         VideoResponse videoResponse = TaskResponse.getBody();
 
         return videoResponse;
